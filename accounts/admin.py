986c9c84cccc0833448b48bbbad0620197bce1a3
wojtek <wojtek.klusek2@gmail.com>
2019-04-29 20:25:18 +0200

    edit admin.py

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from accounts import models
 
 # Register your models here.
+
+admin.site.register(models.UserProfileInfo)
+admin.site.register(models.MyUser)
+
--- ems_app/admin.py
+++ ems_app/admin.py
@@ -1,7 +1,3 @@
 from django.contrib import admin
-from accounts import models
 
 # Register your models here.
-
-
-admin.site.register(models.UserProfileInfo)
